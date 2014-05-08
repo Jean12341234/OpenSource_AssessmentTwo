33d49b4f98773ebb8abce0b2d79b2d09f855148c
Jean12341234 <geanemanuel@yahoo.com>
2014-05-08 15:49:49 +0800

    Update Main.java
    By: Jean & Gerald

--- Main.java
+++ Main.java
@@ -1,24 +1,46 @@
+package fuelpurchase;
 import java.util.*;
-
+/**
+ *
+ * @author 041209329
+ */
 public class Main {
-
-	/**
+        /**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Vehicle v = new Vehicle("Ford", "T812", 2014);
-
-		v.printDetails();
+		System.out.println("           >>> THE FUEL ECONOMY <<<");
+                System.out.println("Audio A6, Jaguar X200 & Mercedes-Benz CL600 C220");
+                System.out.println("------------------------------------------------");
+                
+                //Gerakd added some vehicles
+		Vehicle A = new Vehicle("Audi", "A6", 2014);
+                Vehicle J = new Vehicle ("Jaguar", "X200", 2013);
+                Vehicle M = new Vehicle ("Mercedes-Benz", "CL600-C220", 2014);
 		
-		// Vehicle sample distance
+		//Jean arranged the codes by their Vehicle class A,J,M
+		// Vehicle A distance
 		for (int i = 0; i < 10; i++) {
-			v.addKilometers(new Random().nextInt(100));
+			A.addKilometers(new Random().nextInt(100));
 		}
-		
-		System.out.println("\n\n");
-		
-		v.printDetails();
-	}
+                System.out.println("\n\n");
+                A.printDetails();
+                
+                // Vehicle J distance
+                for (int i = 0; i < 10; i++) {
+                        J.addKilometers(new Random().nextInt(100));
+                }
+                System.out.println("\n\n");
+                J.printDetails();
+                
+                // Vehicle M distance
+                for (int i = 0; i < 10; i++) {
+                        M.addKilometers(new Random().nextInt(100));
+                }
+                System.out.println("\n\n");
+                M.printDetails();
+        
+        
+        }
 
 }
