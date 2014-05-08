579c002acbd674e81aafdffe684ab2c0f346c3f9
Jean12341234 <geanemanuel@yahoo.com>
2014-05-08 15:40:19 +0800

    Update Journey.java
    By; Jean & Gerald

--- Journey.java
+++ Journey.java
@@ -1,14 +1,28 @@
 
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fuelpurchase;
+//Jean added a package to generate random
+import java.util.Random;
+
+/**
+ *
+ * @author 041209329
+ */
 public class Journey {
+    
 	private static final int	SERVICE_KILOMETER_LIMIT	= 100;
 	private double				kilometers;
-
+        private double                            litres;
 	/**
 	 * Class constructor
 	 */
 	public Journey() {
 		this.kilometers = 0;
-	}
+                this.litres = 0;
+        }
 
 	/**
 	 * Appends the distance parameter to {@link #kilometers}
@@ -30,14 +44,25 @@ public class Journey {
 	 */
 	public int getTotalServices() {
 		return (int) Math.floor(kilometers / SERVICE_KILOMETER_LIMIT);
-	}
+        }
+       
+        
 
 	/**
 	 * 
-	 * @return {@link #kilometers}
+	 * @return {@link #kilometers #litres}
 	 */
 	public double getKilometers() {
 		return kilometers;
-	}
+        }
+        //Jeans change
+        public double getLiters() {
+                return (new Random().nextInt(100) * 100 / kilometers);
+        }
+        
+        // Gerald add this code to ger fuel consumption
+        public double fuelEconomy() {
+        	return kilometers / getliters;
+        }
 
 }
